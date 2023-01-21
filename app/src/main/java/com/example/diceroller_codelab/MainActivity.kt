0c66466682435ba5d8fdb1a326922f691c7ed507
flavorbreaker <turner.l.anthony@gmail.com>
2023-01-21 10:26:32 -0500

    added text of result above die image

--- app/src/main/java/com/example/diceroller_codelab/MainActivity.kt
+++ app/src/main/java/com/example/diceroller_codelab/MainActivity.kt
@@ -6,16 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller_codelab.ui.theme.DiceRoller_CodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +48,14 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = result.toString(),
+            color = Color.Magenta,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+
+        )
+        
         Image(painter = painterResource(imageResource), contentDescription = result.toString())
 
         Spacer(modifier = Modifier.height(16.dp))
